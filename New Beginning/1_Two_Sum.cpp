ce1e209480b40f7518fe5ef83126068d23b2178d
navidsami-1 <navidsami05@gmail.com>
2026-07-17 11:07:02 +0600

    TwoSumLeed.cpp

---
+++ New Beginning/1_Two_Sum.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mp;
+
+        for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+
+            if (mp.find(complement) != mp.end()) {
+                return {mp[complement], i};
+            }
+
+            mp[nums[i]] = i;
+        }
+
+        return {};
+    }
+};
+
+int main() {
+    Solution sol;
+
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+
+    vector<int> ans = sol.twoSum(nums, target);
+
+    cout << ans[0] << " " << ans[1] << endl;
+
+    return 0;
+}
